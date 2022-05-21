f793ba9c4571dcf7cc9acc3644aaf00157260c50
Franco De Leon <francodeleon97@hotmail.com>
2022-05-20 22:20:46 -0500

    add nft grid layout and button functionality

--- assets/js/blockchains.js
+++ assets/js/blockchains.js
@@ -3,61 +3,14 @@ fetch('./assets/mocks/blockchains.json')
   .then(response => response.json())
   .then(data => {
     const listRef = document.querySelector('#blockchains-list');
+    const btnRef = document.querySelector('#apply-btn');
 
-
-    listRef.addEventListener('change', (e) => {
-      console.log(e.target.value)
-
+    btnRef.addEventListener('click', (e) => {
       const wallet = document.querySelector('input').value;
-      const chain = e.target.value;
-      const url = `https://deep-index.moralis.io/api/v2/${wallet}/nft?chain=${e.target.value}&format=decimal`;
+      const chain = listRef.value;
+      const url = `https://deep-index.moralis.io/api/v2/${wallet}/nft?chain=${chain}&format=decimal`;
 
-      const API_KEY = 'XUnDBl1fLvCROuwpgxpB645C1VrrjGGwfUDz6NmdJNo97qUCftf3a8TU0DGIu6Yo';
-      const options = {
-        method: 'GET',
-        headers: new Headers({ 'X-API-Key': API_KEY }),
-    };
-    
-      fetch(url, options).then(response => response.json())
-        .then(data => {
-          const ulRef = document.querySelector('#nfts-list');
-
-          const nfts = data.result;
-      
-          let listElements = '';
-          let subListElements='';
-          nfts.forEach((nft) => {
-            const metadata = JSON.parse(nft.metadata);
-            if (!metadata) return;
-
-            Object.keys(metadata).map(key => {
-              metadata[key]==null?"":
-
-              subListElements+=`
-                <div class="container">
-                <h1>${nft.token_id}</h1>
-                <div class="row">
-                  <div class="col-sm">
-                  ${metadata[key]==null?"":key}
-                  </div>
-                  <div class="col-sm">
-                  ${
-                    metadata[key]==null?"":
-                             key.includes("image")?`<img width="50%" src="${metadata[key]}" />`:metadata[key]
-                  }
-                  </div>
-                </div>
-              </div>
-              `;
-              ;
-
-             });
-
-
-          });
-
-          ulRef.innerHTML = subListElements
-        })
+      fetchNfts(url);
     });
 
     const blockchains = data;
@@ -69,6 +22,44 @@ fetch('./assets/mocks/blockchains.json')
     });
 
     listRef.innerHTML = listElements;
-
-    console.log(data)
   });
+
+  function fetchNfts(url) {
+    const API_KEY = 'XUnDBl1fLvCROuwpgxpB645C1VrrjGGwfUDz6NmdJNo97qUCftf3a8TU0DGIu6Yo';
+    const options = {
+      method: 'GET',
+      headers: new Headers({ 'X-API-Key': API_KEY }),
+    };
+  
+    fetch(url, options).then(response => response.json())
+      .then(data => {
+        const ulRef = document.querySelector('#nfts-list');
+
+        const nfts = data.result;
+    
+        let subListElements='';
+        ulRef.innerHTML = subListElements;
+
+        nfts.forEach((nft) => {
+          const metadata = JSON.parse(nft.metadata);
+          if (!metadata) return;
+          Object.keys(metadata).map(key => {
+            subListElements+=`
+              <div class="col-md-4" style="min-height: 600px;">
+                <h1 class="truncated">
+                  <span title="${nft.token_address}">${nft.token_address}</span>
+                </h1>
+                ${key.includes("image")
+                  ? `<img style="max-width: 100%;" src="${metadata[key]}" />`
+                  : `<p class="truncated">
+                      <span title="${metadata[key]}">${key}: ${metadata[key]}</span>
+                    </p>`
+                }
+              </div>
+            `;
+            });
+        });
+
+        ulRef.innerHTML = subListElements;
+      })
+  }
